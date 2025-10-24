614cc04a5bc03b297e376d998c35cd6998b6e108
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2025-10-24 16:45:21 +0200

    Working installer script with xattr

--- scripts/create_macos_installer.sh
+++ scripts/create_macos_installer.sh
@@ -1,7 +1,5 @@
 #!/bin/zsh
 
-set -o xtrace
-
 PLUGIN_PATH=$1
 PLUGIN_FILE_NAME=$PLUGIN_PATH:t
 echo "Creating an installer for $PLUGIN_FILE_NAME"
@@ -9,18 +7,13 @@ echo "Creating an installer for $PLUGIN_FILE_NAME"
 DEST_DIR="/Library/Audio/Plug-Ins/VST3"
 
 SCRIPTS_PATH="package_scripts"
-POSTINSTALL_SCRIPTS_PATH="$SCRIPTS_PATH/postinstall"
-mkdir -p $POSTINSTALL_SCRIPTS_PATH
-POSTINSTALL_SCRIPT_PATH="$POSTINSTALL_SCRIPTS_PATH/xattr_plugin.sh"
+POSTINSTALL_SCRIPT_PATH="$SCRIPTS_PATH/postinstall"
+mkdir $SCRIPTS_PATH
 
 echo "#!/bin/zsh" > $POSTINSTALL_SCRIPT_PATH
-echo "set -o xtrace" >> $POSTINSTALL_SCRIPT_PATH
-echo "echo Here >> /Users/jawi/Desktop/debug.txt" >> $POSTINSTALL_SCRIPT_PATH
-echo "sudo xattr -cr $DEST_DIR/$PLUGIN_FILE_NAME" >> $POSTINSTALL_SCRIPT_PATH
-echo "set +o xtrace" >> $POSTINSTALL_SCRIPT_PATH
+echo "sudo xattr -cr $DEST_DIR/$PLUGIN_FILE_NAME\n" >> $POSTINSTALL_SCRIPT_PATH
 
-# pkgbuild --scripts $SCRIPTS_PATH --install-location $DEST_DIR --root $PLUGIN_PATH --component-plist "$PLUGIN_PATH/Contents/Info.plist" "$PLUGIN_FILE_NAME.pkg"
-# pkgbuild --scripts $SCRIPTS_PATH --install-location $DEST_DIR --component $PLUGIN_PATH "$PLUGIN_FILE_NAME.pkg"
-pkgbuild --install-location $DEST_DIR --component $PLUGIN_PATH "$PLUGIN_FILE_NAME.pkg"
+chmod +x $POSTINSTALL_SCRIPT_PATH
 
-set +o xtrace
+pkgbuild --scripts $SCRIPTS_PATH --install-location $DEST_DIR --component $PLUGIN_PATH "$PLUGIN_FILE_NAME.pkg"
+
