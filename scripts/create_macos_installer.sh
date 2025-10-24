6c6c88f54b118ccc951336c9e06d13f2a0679935
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2025-10-24 14:05:35 +0200

    The installer script copies the plugin bundle to /Library/Audio/Plug-Ins/VST3/ successfully

--- scripts/create_macos_installer.sh
+++ scripts/create_macos_installer.sh
@@ -6,7 +6,7 @@ PLUGIN_PATH=$1
 PLUGIN_FILE_NAME=$PLUGIN_PATH:t
 echo "Creating an installer for $PLUGIN_FILE_NAME"
 
-DEST_DIR="~/Library/Audio/Plug-Ins/VST3"
+DEST_DIR="/Library/Audio/Plug-Ins/VST3"
 
 SCRIPTS_PATH="package_scripts"
 POSTINSTALL_SCRIPTS_PATH="$SCRIPTS_PATH/postinstall"
@@ -14,7 +14,10 @@ mkdir -p $POSTINSTALL_SCRIPTS_PATH
 POSTINSTALL_SCRIPT_PATH="$POSTINSTALL_SCRIPTS_PATH/xattr_plugin.sh"
 
 echo "#!/bin/zsh" > $POSTINSTALL_SCRIPT_PATH
+echo "set -o xtrace" >> $POSTINSTALL_SCRIPT_PATH
+echo "echo Here >> /Users/jawi/Desktop/debug.txt" >> $POSTINSTALL_SCRIPT_PATH
 echo "sudo xattr -cr $DEST_DIR/$PLUGIN_FILE_NAME" >> $POSTINSTALL_SCRIPT_PATH
+echo "set +o xtrace" >> $POSTINSTALL_SCRIPT_PATH
 
 # pkgbuild --scripts $SCRIPTS_PATH --install-location $DEST_DIR --root $PLUGIN_PATH --component-plist "$PLUGIN_PATH/Contents/Info.plist" "$PLUGIN_FILE_NAME.pkg"
 # pkgbuild --scripts $SCRIPTS_PATH --install-location $DEST_DIR --component $PLUGIN_PATH "$PLUGIN_FILE_NAME.pkg"
